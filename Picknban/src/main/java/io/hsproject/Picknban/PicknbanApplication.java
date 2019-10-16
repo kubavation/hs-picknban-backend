72bbb5901ece31295d265c8043ee678cb60e617a
kubavation <kubavation@wp.pl>
2019-10-16 20:36:11 +0200

    initial commit

---
+++ Picknban/src/main/java/io/hsproject/Picknban/PicknbanApplication.java
@@ -0,0 +1,13 @@
+package io.hsproject.Picknban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicknbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicknbanApplication.class, args);
+	}
+
+}
