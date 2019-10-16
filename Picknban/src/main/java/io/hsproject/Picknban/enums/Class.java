b0348f7ac60dbf6b58ead1eac88b646aac30fd9a
kubavation <kubavation@wp.pl>
2019-10-16 20:48:06 +0200

    Class enum added

---
+++ Picknban/src/main/java/io/hsproject/Picknban/enums/Class.java
@@ -0,0 +1,23 @@
+package io.hsproject.Picknban.enums;
+
+public enum Class {
+    DRUID("Druid"),
+    HUNTER("Hunter"),
+    WARRIOR("Warrior"),
+    ROGUE("Rogue"),
+    MAGE("Mage"),
+    PALLADIN("Palladin"),
+    PRIEST("Priest"),
+    SHAMAN("Shaman"),
+    WARLOCK("Warlock");
+
+    private final String name;
+
+    Class(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
