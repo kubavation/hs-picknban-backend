fc0c426a47dcd7f8620fcc3ee5e52714a899c616
kubavation <kubavation@wp.pl>
2019-10-17 17:03:32 +0200

    init room entity

--- Picknban/src/main/java/io/hsproject/Picknban/model/Room.java
+++ Picknban/src/main/java/io/hsproject/Picknban/model/Room.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+
 @Document(collection = "ROOM")
 @Data
 @NoArgsConstructor
@@ -13,4 +15,16 @@ public class Room {
 
     @Id
     private String id;
+
+    private String creator;
+    private String creatorId;
+
+    private String guest;
+    private String guestId;
+
+
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresOn;
+    private boolean isActive;
+
 }
