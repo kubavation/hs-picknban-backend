079d6a594eec506154e1944b8a9f4dc5becd5936
kubavation <kubavation@wp.pl>
2019-10-18 09:43:20 +0200

    creating room service/controller

--- Picknban/src/main/java/io/hsproject/Picknban/controller/RoomController.java
+++ Picknban/src/main/java/io/hsproject/Picknban/controller/RoomController.java
@@ -1,12 +1,26 @@
 package io.hsproject.Picknban.controller;
 
+import io.hsproject.Picknban.dto.RoomDTO;
+import io.hsproject.Picknban.model.Room;
 import io.hsproject.Picknban.service.RoomService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequiredArgsConstructor
 public class RoomController {
 
     private final RoomService roomService;
+
+
+    @PostMapping("/rooms")
+    public ResponseEntity<Room> create(@RequestBody RoomDTO roomDTO){
+        return new ResponseEntity<>(roomService.createRoom(room), CREATED);
+    }
 }
--- Picknban/src/main/java/io/hsproject/Picknban/service/RoomService.java
+++ Picknban/src/main/java/io/hsproject/Picknban/service/RoomService.java
@@ -1,5 +1,7 @@
 package io.hsproject.Picknban.service;
 
+import io.hsproject.Picknban.dto.RoomDTO;
+import io.hsproject.Picknban.model.Room;
 import io.hsproject.Picknban.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,4 +13,7 @@ public class RoomService {
     private final RoomRepository roomRepository;
 
 
+    public Room createRoom(Room room) {
+        return roomRepository.save(room);
+    }
 }
