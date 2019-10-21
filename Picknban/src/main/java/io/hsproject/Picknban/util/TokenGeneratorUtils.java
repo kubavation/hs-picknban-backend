df13b5f4439e571f08d39d72944390f234bf8cf6
kubavation <kubavation@wp.pl>
2019-10-21 09:57:45 +0200

    tokenGeneratorUtils class

--- Picknban/src/main/java/io/hsproject/Picknban/util/TokenGeneratorUtils.java
+++ Picknban/src/main/java/io/hsproject/Picknban/util/TokenGeneratorUtils.java
@@ -1,5 +1,13 @@
 package io.hsproject.Picknban.util;
 
+import io.hsproject.Picknban.enums.UserType;
+import lombok.NonNull;
+
+import java.util.UUID;
+
 public class TokenGeneratorUtils {
 
+    public static String generate(@NonNull String roomId, UserType type) {
+        return type.getShortcut() + "_" + roomId + "_" + UUID.randomUUID().toString();
+    }
 }
